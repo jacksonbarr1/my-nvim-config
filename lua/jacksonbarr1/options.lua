5a6bd54b4f0dde7df382d0487006399f75eb509c
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-20 15:24:47 -0400

    Configure options.lua

---
+++ lua/jacksonbarr1/options.lua
@@ -0,0 +1,38 @@
+local g = vim.g
+local o = vim.o
+
+g.mapleader = " "
+g.maplocalleader = " "
+
+g.have_nerd_font = true
+
+o.number = true
+o.relativenumber = true
+
+o.mouse = "a"
+
+o.clipboard = "unnamedplus"
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+o.smartindent = true
+
+o.wrap = false
+
+o.swapfile = false
+o.backup = false
+o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undofile = true
+
+o.hlsearch = false
+o.incsearch = true
+
+o.termguicolors = true
+
+o.scrolloff = 8
+o.signcolumn = "yes"
+
+o.updatetime = 50
