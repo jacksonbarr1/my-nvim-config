eb089ec70f5a4e2feff7a176c6d092a86a93520d
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-20 16:53:18 -0400

    Fix incorrect builtin reference

--- lua/jacksonbarr1/plugins/telescope.lua
+++ lua/jacksonbarr1/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
 		require("telescope").setup({})
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.fild_files, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 	end,
