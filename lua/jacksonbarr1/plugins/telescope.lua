40d8f6f52246fe6ef427d1a742374859a09ea4cc
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-20 17:08:00 -0400

    Use vertical layout for telescope pickers

--- lua/jacksonbarr1/plugins/telescope.lua
+++ lua/jacksonbarr1/plugins/telescope.lua
@@ -4,7 +4,11 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+            layout_config = {
+                vertical = {}
+            }
+        })
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
