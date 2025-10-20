3dcce7cc1b28511321df9e69c51ad731f84d737c
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-20 16:46:31 -0400

    Configure theme

---
+++ lua/jacksonbarr1/plugins/onedark.lua
@@ -0,0 +1,18 @@
+return {
+	"navarasu/onedark.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("onedark").setup({
+			style = "dark",
+			transparent = true,
+			term_colors = true,
+
+			lualine = {
+				transparent = true,
+			},
+
+			require("onedark").load(),
+		})
+	end,
+}
